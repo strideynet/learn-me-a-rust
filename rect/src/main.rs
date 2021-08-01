d0232312cb4f0370843d149d2485e0c959ca3ddb
Noah Stride <noah@noahstride.co.uk>
2021-08-01 11:55:06 +0100

    more rectangles!

--- rect/src/main.rs
+++ rect/src/main.rs
@@ -1,5 +1,5 @@
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 struct Point (i32, i32);
 
 #[derive(Debug)]
@@ -34,12 +34,42 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width() * self.height()
     }
+
+    fn is_bigger(&self, other: &Rectangle) -> bool {
+        self.area() > other.area()
+    }
+
+    fn square(top_corner: Point, side_length: u32) -> Rectangle {
+        Rectangle{
+            top_corner: top_corner,
+            bottom_corner: Point(
+                top_corner.0 + side_length as i32, 
+                top_corner.1 + side_length as i32,
+            ),
+        }
+    }
 }
 
 fn main() {
-    let r = Rectangle{
+    let r = Rectangle {
         top_corner: Point(0, 0),
-        bottom_corner: Point(10, 10)
+        bottom_corner: Point(10, 10),
     };
-    println!("Hello, world! {}", r.area());
+
+    let r2 = Rectangle {
+        top_corner: Point(0, 0),
+        bottom_corner: Point(5, 5),
+    };
+
+    let r3 = Rectangle {
+        top_corner: Point(0, 0),
+        bottom_corner: Point(30, 5),
+    };
+
+    let sq = Rectangle::square(Point(0, 1), 64);
+    println!("sq {:?}", sq);
+
+    println!("Hello, world! {}", sq.area());
+    println!("he fits {}", r.is_bigger(&r2));
+    println!("he fits {}", r.is_bigger(&r3));
 }
